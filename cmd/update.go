461ee1525822c3c06bd20bfccd27dac8d2b5277e
Grégoire Détrez <gregoire@mullvad.net>
2022-05-06 15:28:42 +0200

    Tearch "update" a new "--all" option
    "update" learned a new option "--all" that updates all files git-carbon
    knowns about.

--- cmd/update.go
+++ cmd/update.go
@@ -8,10 +8,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 
 	"github.com/gregoire-mullvad/git-carbon/config"
 	"github.com/spf13/cobra"
 )
+
+var allFlag *bool
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+	allFlag = updateCmd.Flags().BoolP("all", "a", false, "Update all files git-carbon knows about.")
+}
 
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
@@ -21,7 +29,15 @@ var updateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		conf, err := config.Load()
 		die(err)
-		for _, p := range args {
+		paths := args
+		if *allFlag {
+			paths = make([]string, 0, len(conf.CCs))
+			for name := range conf.CCs {
+				paths = append(paths, name)
+			}
+			sort.Strings(paths)
+		}
+		for _, p := range paths {
 			cc := conf.CCs[p]
 			fmt.Fprintf(os.Stderr, "Updating %s from %s\n", p, cc.SourceRepository)
 			src, err := getSourceFile(p, cc.SourceRepository)
@@ -35,9 +51,4 @@ var updateCmd = &cobra.Command{
 			die(err)
 		}
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(updateCmd)
-	updateCmd.Flags().BoolP("all", "a", false, "Update all files git-carbon knows about.")
 }
---
+++ tests/update-all.t
@@ -0,0 +1,36 @@
+  $ source "$TESTDIR/__init__.sh"
+
+
+Init source repository:
+
+  $ git init --quiet A
+  $ echo "READMEv1" > A/README.md
+  $ echo "FOOBARv1" > A/FOOBAR.md
+  $ git -C A add README.md FOOBAR.md
+  $ git -C A commit -qm "Initial commit"
+
+Init dest repository:
+
+  $ git init --quiet B
+
+Add carbon copies
+
+  $ git -C B carbon add README.md ../A
+  $ git -C B carbon add FOOBAR.md ../A
+  $ git -C B commit -qm "Add carbon copies"
+
+Update upstream files
+
+  $ echo "READMEv2" > A/README.md
+  $ echo "FOOBARv2" > A/FOOBAR.md
+  $ git -C A commit -qam "Initial commit"
+
+Update all copies
+
+  $ git -C B carbon update --all
+  Updating FOOBAR.md from ../A
+  Updating README.md from ../A
+  $ cat B/README.md
+  READMEv2
+  $ cat B/FOOBAR.md
+  FOOBARv2
